5322fabf7836aaa46446d711d31e48d3439094f7
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2023-01-31 06:54:20 +0200

    Update test_Numbers.php

--- tests/test_Numbers.php
+++ tests/test_Numbers.php
@@ -54,11 +54,12 @@ class Test_Numbers extends PHPUnit\Framework\TestCase
         //FR -- 1
         // ES -- 2
         // "DE" -- 8
-        // Ru == 54
+        // Ru -- 54
         // Pt -- 54
+        // Fa -- 54
 
 
-        $languages = array("FA");
+        $languages = array("KO");
 
         // add NULL & '' to test cases
         $numbers = array("one", "1.2.30", 0, 1, 2, 3, 10, 11, 12, 13, 20, 41, 70, 71, 73, 82, 100, 101, 200, 600, 1000, 1001, 2000, 9000, 10000, 10001, 13000, 30000,
