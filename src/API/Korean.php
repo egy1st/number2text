952618dd60b16d692a992401e33993f9eee587e5
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2023-02-02 16:49:26 +0200

    up

--- src/API/Korean.php
+++ src/API/Korean.php
@@ -27,7 +27,7 @@ class Korean
         $KOR = new Korean();
         $strNum = "";
         $N[17] = 0;
-        NumberingSystem::getLanguage($aUnit, $aTen, $aHundrd, $aId, $aNum, "Korean");
+        NumberingSystem::getLanguage($aUnit, $aTen, $aHundred, $aId, $aNum, "Korean");
         for ($x = 7; $x <= 12; $x++) {
             $M [$x] = $aCur [$x - 7];
         }
@@ -35,7 +35,7 @@ class Korean
         //=====================================================================
         // each cycle represents a scale hunderds and tens, thousnads, millions and milliars
         $cycle = 0;
-        for ($cycle = 1; $cycle <= 4; $L++) {
+        for ($cycle = 1; $cycle <= 4; $cycle++) {
             if ($cycle === 1) {
                 $x = 1;
             } else if ($cycle === 2) {
@@ -50,7 +50,7 @@ class Korean
             // Prepre numbers from 0 to 99
             // Tens and units are linked with e (and), as in trinta e cinco [35]
 
-            $strForma = Number2Text::prepareNumber($strNumber, $N);
+            $strForma = Number2Text::prepareNumber($strNumber, $aNum);
 
             $y = 0;
             $ptrn = $aNum[$x] . $aNum[$x + 1] . $aNum[$x + 2] . $aNum[$x + 3];
@@ -100,20 +100,20 @@ class Korean
         }
 
         //echo "<br>" .$strNum ;
-        return $Num;
+        return $srtNum;
 
     }
 
     public static function getGrand($L)
     {
 
-        if ($cycle == 1) {
+        if ($cycle === 1) {
             return "억 ";
             // 100 Million
-        } else if ($cycle == 2) {
+        } else if ($cycle === 2) {
             return "만 ";
             // Ten Thousands
-        } else if ($cycle == 3) {
+        } else if ($cycle === 3) {
             return "";
             // units
         }
@@ -128,13 +128,13 @@ class Korean
     public static function getID($y)
     {
 
-        if ($y % 4 == 1) {
+        if ($y % 4 === 1) {
             return "천";
             // Thousands
-        } else if ($y % 4 == 2) {
+        } else if ($y % 4 === 2) {
             return "백";
             // Hundereds
-        } else if ($y % 4 == 3) {
+        } else if ($y % 4 === 3) {
             return "십";
             // Tens
         }
@@ -149,9 +149,9 @@ class Korean
     {
         $NS = new NumberingSystem();
 
-        if ($cycle == 1 & NumberingSystem::isPattern($strForma, "x1xxxxxxxxxx.xxxx")) {
+        if ($cycle === 1 & NumberingSystem::isPattern($strForma, "x1xxxxxxxxxx.xxxx")) {
             return true;
-        } else if ($cycle == 2 & NumberingSystem::isPattern($strForma, "xxxxx1xxxxxx.xxxx")) {
+        } else if ($cycle ===== 2 & NumberingSystem::isPattern($strForma, "xxxxx1xxxxxx.xxxx")) {
             return true;
         } else if ($cycle == 3 & NumberingSystem::isPattern($strForma, "xxxxxxxxx1xx.xxxx")) {
             return true;
@@ -168,13 +168,13 @@ class Korean
 
         $NS = new NumberingSystem();
 
-        if ($cycle == 1 & NumberingSystem::isPattern($strForma, "xx1xxxxxxxxx.xxxx")) {
+        if ($cycle === 1 & NumberingSystem::isPattern($strForma, "xx1xxxxxxxxx.xxxx")) {
             return true;
-        } else if ($cycle == 2 & NumberingSystem::isPattern($strForma, "xxxxxx1xxxxx.xxxx")) {
+        } else if ($cycle === 2 & NumberingSystem::isPattern($strForma, "xxxxxx1xxxxx.xxxx")) {
             return true;
-        } else if ($cycle == 3 & NumberingSystem::isPattern($strForma, "xxxxxxxxxx1x.xxxx")) {
+        } else if ($cycle === 3 & NumberingSystem::isPattern($strForma, "xxxxxxxxxx1x.xxxx")) {
             return true;
-        } else if ($cycle == 4 & NumberingSystem::isPattern($strForma, "xxxxxxxxxxxx.xx1x")) {
+        } else if ($cycle === 4 & NumberingSystem::isPattern($strForma, "xxxxxxxxxxxx.xx1x")) {
             return true;
         }
 
@@ -185,15 +185,15 @@ class Korean
     {
         $NS = new NumberingSystem();
 
-        if ($cycle == 1 & NumberingSystem::isPattern($strForma, "xxx1xxxxxxxx.xxxx")) {
+        if ($cycle === 1 & NumberingSystem::isPattern($strForma, "xxx1xxxxxxxx.xxxx")) {
             return true;
-        } else if ($cycle == 2 & NumberingSystem::isPattern($strForma, "xxxxxxx1xxxx.xxxx")) {
+        } else if ($cycle === 2 & NumberingSystem::isPattern($strForma, "xxxxxxx1xxxx.xxxx")) {
             return true;
             // not applied here
-        } else if ($cycle == 3) {
+        } else if ($cycle === 3) {
             return false;
             // not applied here
-        } else if ($cycle == 4) {
+        } else if ($cycle === 4) {
             return false;
         }
 
@@ -205,14 +205,14 @@ class Korean
 
         $NS = new NumberingSystem();
 
-        if ($cycle == 1 & NumberingSystem::isPattern($strForma, "1xxxxxxxxxxx.xxxx")) {
+        if ($cycle === 1 & NumberingSystem::isPattern($strForma, "1xxxxxxxxxxx.xxxx")) {
             return true;
-        } else if ($cycle == 2 & NumberingSystem::isPattern($strForma, "xxxx1xxxxxxx.xxxx")) {
+        } else if ($cycle === 2 & NumberingSystem::isPattern($strForma, "xxxx1xxxxxxx.xxxx")) {
             return true;
-        } else if ($cycle == 3 & NumberingSystem::isPattern($strForma, "xxxxxxxx1xxx.xxxx")) {
+        } else if ($cycle === 3 & NumberingSystem::isPattern($strForma, "xxxxxxxx1xxx.xxxx")) {
             return true;
             // no place in pences places
-        } else if ($cycle == 4) {
+        } else if ($cycle === 4) {
             return false;
         }
 
