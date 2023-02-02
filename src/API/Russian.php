c2f6a12e8e7147663e4bb977b269b5759c0318f5
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2023-02-02 16:46:00 +0200

    Update Russian.php

--- src/API/Russian.php
+++ src/API/Russian.php
@@ -59,7 +59,7 @@ class Russian
             $strForma = Number2Text::prepareNumber($strNumber, $aNum);
 
             $nUnit = ( $aNum[$x + 1] * 10) +  $aNum[$x + 2];
-            $n_all =  $aNum[$x] + $nUnit;
+            $nAll =  $aNum[$x] + $nUnit;
             // keywords
             if ($nUnit > 0 & $nUnit < 21) {
                 $strUnit = $aUnit[$nUnit];
