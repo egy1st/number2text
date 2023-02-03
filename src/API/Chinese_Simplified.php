bac5828b8b3566a31829bc4f7bd6479f55c191b2
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2023-02-03 15:22:37 +0200

    Update Chinese_Simplified.php

--- src/API/Chinese_Simplified.php
+++ src/API/Chinese_Simplified.php
@@ -73,7 +73,7 @@ class Chinese_Simplified
 
             $y = 0;
 
-            //if (isset($aNum[$x + 3])) 
+            if (isset($aNum[$x + 2])) 
             {
                 $ptrn = $aNum[$x] . $aNum[$x + 1] . $aNum[$x + 2] . $aNum[$x + 3];
 
