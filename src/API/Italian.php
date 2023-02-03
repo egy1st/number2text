41ffab7040c67e1f361d48431c7bab3b4abbcb7e
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2023-02-03 11:03:11 +0200

    up

--- src/API/Italian.php
+++ src/API/Italian.php
@@ -3,8 +3,6 @@
 // ini_set("display_errors", 1);
 // ini_set('error_reporting', E_ALL);
 
-require_once "NumberingSystem.php";
-require_once "Number2Text.php";
 
 /**
  * @covers Italian
@@ -21,7 +19,7 @@ class Italian
      *
      * @return string
      */
-    public function TranslateNumber($strNumber, $aCur)
+    public function translateNumber($strNumber, $aCur)
     {
         $ITA = new Italian ();
         $strNum = "";
