a3a0eb4a0b6c1a61e100bf2ec61c70c0804fb7a2
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2023-02-03 07:54:53 +0200

    Update Number2Text.php

--- src/API/Number2Text.php
+++ src/API/Number2Text.php
@@ -56,7 +56,7 @@ class Number2Text
             }
 
             $strForma = self::formatNumber($strNumber);
-      
+            $strNum = ""; 
             $E = 0;
             for ($E = 0; $E < 12; $E++) {
                 $S = substr($strForma, $E, 1);
