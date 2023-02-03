c64df01d725bdc7f70aa81b5e43885abed0065fe
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2023-02-03 10:19:15 +0200

    Update test_Numbers.php

--- tests/test_Numbers.php
+++ tests/test_Numbers.php
@@ -180,6 +180,7 @@ class Test_Numbers extends PHPUnit\Framework\TestCase
 		//$strNumber, $language, $locale, $currency, $units, $output
         $languages = ["AR", "EN", "FR", "DE", "IT", "ES", "PT", "TR", "FA", "RU", "KO", "ZH_TW", "ZH_CN"]; 
         $languages = ["EN"]; 
+        
 
         
         // Add null & '' to test cases
