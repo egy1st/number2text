da2dc24b4946be81d713b222b6eb464362fff9ec
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2023-02-03 17:25:47 +0200

    Update Chinese_Simplified.php

--- src/API/Chinese_Simplified.php
+++ src/API/Chinese_Simplified.php
@@ -75,7 +75,7 @@ class Chinese_Simplified
              if (isset($aNum[$x + 3])) {
 
                 $ptrn = $aNum[$x] . $aNum[$x + 1] . $aNum[$x + 2] . $aNum[$x + 3];
-             }
+             
                 $i = 0;
                 for ($y = $x; $y <= $x + 3; $y++) {
                     $i += 1;
@@ -101,7 +101,7 @@ class Chinese_Simplified
                         }
 
                     }
-                
+                }
             }
 
             if ($ptrn != "0000") {
