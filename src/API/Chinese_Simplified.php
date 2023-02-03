8da456e11e8c72da2800899beee3132551d1c8b1
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2023-02-03 10:20:41 +0200

    Update Chinese_Simplified.php

--- src/API/Chinese_Simplified.php
+++ src/API/Chinese_Simplified.php
@@ -125,7 +125,7 @@ class Chinese_Simplified
 
         // Num = removeComma(Num) ' no comma is used in Finnish
        // $strNum = NumberingSystem::removeSpaces($strNum);
-        $strNum = NumberingSystem::removeAnd($strNum, $aId[0]);
+       // $strNum = NumberingSystem::removeAnd($strNum, $aId[0]);
 
         if ($strForma === "000000000000.0000") {
             $strNum = $aUnit[0];
