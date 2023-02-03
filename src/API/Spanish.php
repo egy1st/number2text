115c67159641f1aa5a0e831a3b5c5196e2575c60
Mohammad Ali Abbas <mohamed.alyabbas@outlook.com>
2023-02-03 06:54:22 +0200

    Update Spanish.php

--- src/API/Spanish.php
+++ src/API/Spanish.php
@@ -30,6 +30,7 @@ class Spanish
         for ($x = 7; $x <= 12; $x++) {
             $aId[$x] = $aCur [$x - 7];
         }
+        
 
         // ====================================================================
         // Each cycle represent a scale hunderds and tens, thousnads, millions and milliars
